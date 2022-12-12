fa6ba54ed30c4b8526405e09a1cbbcb220b8068b
azariahpundari1 <thenightman202@gmail.com>
2022-12-12 14:10:00 +1000

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+      // the println() method - prints a new line with every statment
+      System.out.println("Println() examples");
+      System.out.println("LOL");
+      System.out.println("Hi");
+
+      // the print() method - notice it does not print a new line after each statememnt
+      System.out.println("Print() examples");
+      System.out.print("Hello World! ");
+      System.out.print("This will be on the same line");
+      System.out.println("");
+
+      // printing numerical values
+      System.out.println("Print numbers example: ");
+      System.out.println(330);
+      System.out.println(201);
+    }
+  }
